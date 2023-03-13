7ddd89966a9d0ff738c46d5835bc13c0f5da8dc8
Himanshu Kumar <agarwalhimanshugaya@gmail.com>
2023-03-14 01:47:10 +0530

    adding user.js models

---
+++ backened/models/user.js
@@ -0,0 +1,31 @@
+const mongoose=require("mongoose");
+const jwt=require("jsonwebtoken");
+const joi=require("joi");
+const passwordComplexity=require("joi-password-complexity");
+const {string} =require("joi");
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    collegeName:{type:string,required:true},
+});
+userSchema.methods.generateAuthToken=function(){
+    const token=jwt.sign(
+        {_id:this._id,name:this.name},
+        process.env.JWTPRIVATEKEY,
+        {expiresIn:"7d"}
+    )
+    return token;
+}
+const validate=(user)=>{
+    const schema=joi.object({
+        name:joi.string().min(5).max(10).required(),
+        email:joi.string().required(),
+        password:passwordComplexity().required(),
+        collegeName:joi.string().min(4).max(20).required(),
+    
+    });
+    return schema.validate(user);
+}
+const User=mongoose.model("user",userSchema);
+module.exports={User,validate};
